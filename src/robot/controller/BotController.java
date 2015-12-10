fff143cb69aaa7bb369a4cf9e8a71635cea792a6
Adam Kinzer <adamkinzer915@gmail.com>
2015-12-10 13:54:18 -0700

    Edit

--- src/robot/controller/BotController.java
+++ src/robot/controller/BotController.java
@@ -1,6 +1,36 @@
 package robot.controller;
 
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+import robot.model.EV3Bot;
+
+
 public class BotController
 {
-
+	private String message;
+	
+	private int xPosition;
+	private int yPosition;
+	
+	private long waitTime;
+	
+	private EV3Bot AdamBot;
+	
+	public BotController()
+	{
+		this.message = "Adam's Robot arrrg";
+		this.xPosition = 10;
+		this.yPosition = 10;
+		this.waitTime = 4000;
+		
+		AdamBot = new EV3Bot();
+	}
+	
+	public void strat()
+	{
+		LCD.drawString(message, xPosition, yPosition);
+		Delay.msDelay(waitTime);
+		
+		AdamBot.driveRoom();
+	}
 }
