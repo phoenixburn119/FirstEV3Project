ba5afd695bb179b55887b58e4c4049188b203b45
Adam Kinzer <adamkinzer915@gmail.com>
2015-12-14 13:28:34 -0700

    Added some of everything

--- src/robot/controller/AdamBotRunner.java
+++ src/robot/controller/AdamBotRunner.java
@@ -2,5 +2,9 @@ package robot.controller;
 
 public class AdamBotRunner
 {
-
+	public static void main (String [] args)
+	{
+		BotController myBotController = new BotController();
+		myBotController.start();
+	}
 }
--- src/robot/model/EV3Bot.java
+++ src/robot/model/EV3Bot.java
@@ -1,6 +1,58 @@
 package robot.model;
 
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.Motor;
+import lejos.utility.Delay;
+
 public class EV3Bot
 {
-
+	private String botMessage;
+	private int xPosition;
+	private int yPosition;
+	private long waitTime;
+	
+	public EV3Bot()
+	{
+		this.botMessage = "You know where it is";
+		this.xPosition = 50;
+		this.yPosition = 50;
+		this.waitTime = 4000;
+	}
+	
+	public void driveRoom()
+	{
+		displayMessage();
+		Delta();
+		Alpha();
+	}
+	
+	public void displayMessage()
+	{
+		LCD.drawString(botMessage, xPosition, yPosition);
+		Delay.msDelay(waitTime);
+	}
+	private void displyMessage(String message)
+	{
+		LCD.drawString(message, xPosition, yPosition);
+		Delay.msDelay(waitTime);
+	}
+	
+	private void Delta()
+	{
+		Motor.A.setSpeed(900);
+		Motor.B.setSpeed(900);
+		Delay.msDelay(3000);
+		Motor.A.stop();
+		Motor.B.stop();
+	}
+	
+	private void Alpha()
+	{
+		Motor.A.setSpeed(-900);
+		Motor.B.setSpeed(900);
+		Delay.msDelay(3000);
+		Motor.A.stop();
+		Motor.B.stop();
+	}
 }
+
